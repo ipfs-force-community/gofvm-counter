a63fb4051ea30a9528a5e803a7ea185b0e85c77b
hunjixin <1084400399@qq.com>
2022-08-11 15:45:32 +0800

    update template

--- actor/cbor_gen.go
+++ actor/cbor_gen.go
@@ -3,11 +3,13 @@
 package actor
 
 import (
+	"fmt"
 	"io"
 	"math"
 	"sort"
 
 	cid "github.com/ipfs/go-cid"
+	cbg "github.com/whyrusleeping/cbor-gen"
 	xerrors "golang.org/x/xerrors"
 )
 
@@ -19,9 +21,77 @@ var _ = sort.Sort
 var lengthBufState = []byte{129}
 
 func (t *State) MarshalCBOR(w io.Writer) error {
-	panic("left for gen")
+	if t == nil {
+		_, err := w.Write(cbg.CborNull)
+		return err
+	}
+
+	cw := cbg.NewCborWriter(w)
+
+	if _, err := cw.Write(lengthBufState); err != nil {
+		return err
+	}
+
+	// t.Value (int64) (int64)
+	if t.Value >= 0 {
+		if err := cw.WriteMajorTypeHeader(cbg.MajUnsignedInt, uint64(t.Value)); err != nil {
+			return err
+		}
+	} else {
+		if err := cw.WriteMajorTypeHeader(cbg.MajNegativeInt, uint64(-t.Value-1)); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (t *State) UnmarshalCBOR(r io.Reader) (err error) {
-	panic("left for gen")
+	*t = State{}
+
+	cr := cbg.NewCborReader(r)
+
+	maj, extra, err := cr.ReadHeader()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+	}()
+
+	if maj != cbg.MajArray {
+		return fmt.Errorf("cbor input should be of type array")
+	}
+
+	if extra != 1 {
+		return fmt.Errorf("cbor input had wrong number of fields")
+	}
+
+	// t.Value (int64) (int64)
+	{
+		maj, extra, err := cr.ReadHeader()
+		var extraI int64
+		if err != nil {
+			return err
+		}
+		switch maj {
+		case cbg.MajUnsignedInt:
+			extraI = int64(extra)
+			if extraI < 0 {
+				return fmt.Errorf("int64 positive overflow")
+			}
+		case cbg.MajNegativeInt:
+			extraI = int64(extra)
+			if extraI < 0 {
+				return fmt.Errorf("int64 negative oveflow")
+			}
+			extraI = -1 - extraI
+		default:
+			return fmt.Errorf("wrong type for int64 field: %d", maj)
+		}
+
+		t.Value = int64(extraI)
+	}
+	return nil
 }
--- gen/go.mod
+++ gen/go.mod
@@ -3,7 +3,7 @@ module gen
 go 1.16
 
 require (
-	github.com/ipfs-force-community/go-fvm-sdk/gen v0.0.0-20220623030258-7794fbcb5117
+	github.com/ipfs-force-community/go-fvm-sdk/gen v0.0.0-20220810095922-6780bc0983e7
 	gofvm-counter v0.0.0-00010101000000-000000000000
 )
 
